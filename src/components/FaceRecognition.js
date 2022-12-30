50f7cf608cf4f9500da5ce607ea17dd90e9576a0
Pichborith Kong <pichborith@se2nd-air.attlocal.net>
2022-12-29 19:50:54 -0800

    customize

--- src/components/FaceRecognition.js
+++ src/components/FaceRecognition.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import './FaceRecognition.css';
 
 const FaceRecognition = ({ imageUrl }) => {
   return (
     <div className='center ma'>
       <div className='absolute mt2'>
-        <img id='inputimage' alt='' src={imageUrl} width='500px' heigh='auto' />
+        <img alt='' src={imageUrl} width='500px' heigh='auto' />
       </div>
     </div>
   );
