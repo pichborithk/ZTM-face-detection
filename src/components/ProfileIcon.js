a20c3c882367d8ce4ad3f5d8177e55a4366105a2
Pichborith Kong <pichborith@gmail.com>
2023-02-13 13:36:09 -0800

    add user logo and dropdown sign out in home page

---
+++ src/components/ProfileIcon.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import {
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from 'reactstrap';
+
+class ProfileIcon extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dropdownOpen: false,
+    };
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState((prevState) => ({ dropdownOpen: !prevState.dropdownOpen }));
+  }
+
+  render() {
+    return (
+      <div className='pa4 tc'>
+        <Dropdown
+          isOpen={this.state.dropdownOpen}
+          toggle={this.toggle}
+          // direction={direction}
+        >
+          <DropdownToggle
+            tag='span'
+            data-toggle='dropdown'
+            aria-expanded={this.state.dropdownOpen}
+          >
+            <img src='' alt='avatar' className='br-100 ba h3 w3 dib' />
+          </DropdownToggle>
+          <DropdownMenu
+            // {...args}
+            className='b--transparent shadow-5'
+            style={{
+              // marginTop: '20px',
+              backgroundColor: 'rgba(255,255,255,0.5)',
+            }}
+          >
+            <DropdownItem header>User Name</DropdownItem>
+            <DropdownItem>View Profile</DropdownItem>
+            <DropdownItem onClick={() => this.props.onRouteChange('signin')}>
+              Sign Out
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+      </div>
+    );
+  }
+}
+
+export default ProfileIcon;
--- src/components/Register.js
+++ src/components/Register.js
@@ -47,7 +47,7 @@ class Register extends React.Component {
           <main className='pa4 black-80 zindex white'>
             <div className='measure'>
               <fieldset id='sign_up' className='ba b--transparent ph0 mh0'>
-                <legend className='f2 fw6 ph0 mh0'>Sign In</legend>
+                <legend className='f2 fw6 ph0 mh0'>Register</legend>
                 <div className='mt3'>
                   <label className='db fw6 lh-copy f6' htmlFor='name'>
                     Name
